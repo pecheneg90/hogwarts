d79140c4189522d8894dd1b00b5a520c81d8c676
pecheneg90 <100637912+pecheneg90@users.noreply.github.com>
2022-08-12 01:45:50 +0300

    Delete target directory

--- target/classes/liquibase/scripts/index-practice.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
---changeSet pecheneg:1
-
-CREATE INDEX index_student_name ON student (name);
-
---changeSet pecheneg:2
-
-CREATE INDEX index_faculty_name_and_color ON faculty (name, color);
